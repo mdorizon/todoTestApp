74e17a55f9a2408ed3745ab9a55025672cf33d1c
MDorizon <mdorizon@proton.me>
2025-03-31 16:25:28 +0200

    --

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("Verify if page title is correct", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto("/");
   expect(await page.title()).toBe("Vite + React + TS");
 });
